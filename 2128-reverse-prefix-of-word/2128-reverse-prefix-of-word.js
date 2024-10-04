a330249b64b7daf7fd59d1e04b92c922a192bf7a
suhaib <154702605+muhaammedsuhaib@users.noreply.github.com>
2024-10-04 15:58:41 +0530

    Time: 68 ms (6.24%), Space: 49.4 MB (16.72%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.js
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.js
@@ -4,26 +4,14 @@
  * @return {string}
  */
 var reversePrefix = function(word, ch) {
-    let pos=0, val='',rval='',che='';
-  
-    
-   for(let i=0;i<word.length;i++){
-    if(word[i] === ch){
-       pos+=i;
-       for(let j=0;j<=pos;j++){
-        val+= word[j];
-        rval=val.split('').reverse().join('');
+    let rv='';
+let num = word.indexOf(ch);
+for(let i =0;i<=num;i++){
+    rv+= word[i];
 }
-for(let k=pos+1; k<word.length;k++){
-    rval+=word[k];
+val=rv.split('').reverse().join('');
+for(let j=num+1 ;j<word.length;j++){
+    val+=word[j]
 }
-     return rval;
-       break ;
-    }else{
-       che+=word[i];
-    }
-
-
-}
- if(che===word) return word;
+return val
 };
